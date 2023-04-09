f6f7b82bef52bbfcf38cdd9ad821b46bf2fb5e21
Samuel Thomazella Gazzola <samuelgazzola@gmail.com>
2023-04-09 19:04:03 -0300

    Autenticação de formulários e alerta - Aula 04

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from galeria.models import Fotografia
+from django.contrib import messages
 
 def  index(request):
+     if not request.user.is_authenticated:
+          messages.error(request, "Usuário não logado")
+          return redirect("login")
+     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+     return render(request, "galeria/index.html", {"cards": fotografias})
 
-     dados = {
-     1: {"nome": "Nebulosa de Carina",
-         "legenda": "webbtelescope.org / NASA / James Webb"},
-     2: {"nome": "Galáxia NGC 1079",
-         "legenda": "nasa.org / NASA / Hubble"},
-}
+def imagem(request, foto_id):
+     fotografia = get_object_or_404(Fotografia, pk=foto_id)
+     return render(request, "galeria/imagem.html", {"fotografia": fotografia})
 
-     return render(request, "galeria/index.html", {"cards": dados})
+def buscar(request):
+     if not request.user.is_authenticated:
+          messages.error(request, "Usuário não logado")
+          return redirect("login")
+     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
-def imagem(request):
-     return render(request, "galeria/imagem.html")
+     if "buscar" in request.GET:
+          nome_a_buscar = request.GET["buscar"]
+          if nome_a_buscar:
+               fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+     return render(request, "galeria/buscar.html", {"cards": fotografias})
